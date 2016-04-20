11917d974e530057a938f4ecd7bb2d0777841be2
zhewuzhou <zhewuzhou@gmail.com>
2016-04-20 17:57:09 +0800

    [Z.Z][QuickSort] the second test case, get skill that tap to align

--- test/Sort/QuickSortSpec.hs
+++ test/Sort/QuickSortSpec.hs
@@ -3,6 +3,7 @@ module Sort.QuickSortSpec (spec) where
 import Test.Hspec
 import Test.QuickCheck
 
+
 import Sort.QuickSort
 
 spec :: Spec
@@ -10,4 +11,6 @@ spec =
   do describe "quick sort" $
        do it "should sort the simple array" $
             do quicksort [9,8,7,6,5,4,3,2,1] `shouldBe` [1,2,3,4,5,6,7,8,9]
-
+          it "should sort string as well" $
+            do quicksort "the quick brown fox jumps over the lazy dog" `shouldBe`
+                 "        abcdeeefghhijklmnoooopqrrsttuuvwxyz"
