373cf00784de0b966145495e5d3a6be1a906fee7
zhewuzhou <zhewuzhou@gmail.com>
2016-04-20 17:30:06 +0800

    [Z.Z][Haskell] quick sort algorithm

---
+++ src/Sort/QuickSort.hs
@@ -0,0 +1,12 @@
+module Sort.QuickSort (quicksort) where
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []          
+quicksort (x:xs) =
+  let smallerSorted =
+        quicksort [a | a <- xs
+                     , a <= x]
+      biggerSorted =
+        quicksort [a | a <- xs
+                     , a > x]
+  in smallerSorted ++ [x] ++ biggerSorted
---
+++ test/Sort/QuickSortSpec.hs
@@ -0,0 +1,15 @@
+module Sort.QuickSortSpec (main, spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Sort.QuickSort
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec =
+  do describe "quick sort" $
+       do it "should sort the simple array" $
+            do quicksort [9,8,7,6,5,4,3,2,1] `shouldBe` [1,2,3,4,5,6,7,8,9]
